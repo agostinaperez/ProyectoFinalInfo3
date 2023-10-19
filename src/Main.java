cb853a752abfcd2b3eb1cfb6a06e85e28cf204f3
Agostina Pérez <aggostinaperez@gmail.com>
2023-10-19 09:40:00 -0300

    empece el menu de opciones

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        menu();
+
+    }
+
+    public static void menu(){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Bienvenido al sistema de Gestión de Inventario! \n Por favor, seleccione una opción: \n 1.Agregar producto \n 2.");
+        int op=sc.nextInt();
+    }
+
 }
